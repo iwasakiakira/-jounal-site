2e6be5920edf35ba16d58af4526ad2ed3ebc93d6
iwasakiakira <iwasioisiikatuodasi@gmail.com>
2020-10-26 23:45:39 +0900

    add index show

--- server/app/Http/Controllers/ArticleController.php
+++ server/app/Http/Controllers/ArticleController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+//articleクラスを読み込む｡
+use App\article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $article = article::all();
+        // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
+        return view('articles.index', ['article' => $article]);
+    }
+
     public function show($id)
     {
-        # code...
+        $article = article::find($id);
+        return view('articles.show' , ['article' => $article]);
     }
+
+
+
 }
---
+++ server/resources/views/articles/show.blade.php
@@ -0,0 +1,9 @@
+<h1>論文詳細</h1>
+<p>
+<b>タイトル：{{ $article->title }}</b>
+</p>
+
+<p>
+<b>{{ $article->body }}</b>
+</p>
+
