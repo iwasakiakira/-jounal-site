2ecef92e5b56e337ec02250799a4cdf0c2344c2e
iwasakiakira <70826700+iwasakiakira@users.noreply.github.com>
2020-10-27 12:46:43 +0900

    Revert "add index show"

--- server/resources/views/articles/index.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>論文一覧</h1>
-<ul>
-    @foreach ($article as $article)
-        <!-- // リンク先をidで取得し名前で出力 -->
-        <li><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></li>
-    @endforeach
-</ul>
--- server/resources/views/articles/show.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>論文詳細</h1>
-<p>
-<b>タイトル：{{ $article->title }}</b>
-</p>
-
-<p>
-<b>{{ $article->body }}</b>
-</p>
-
